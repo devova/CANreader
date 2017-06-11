6ab1c843e06ba99485e38076f15fc1b02bb3efdf
Volodymyr Trotsyshyn <devova@gmail.com>
2017-06-11 16:54:46 +0300

    fixed fragment init; and send command

--- app/src/main/java/com/autowp/canreader/RadioActivity.java
+++ app/src/main/java/com/autowp/canreader/RadioActivity.java
@@ -4,13 +4,15 @@ import android.graphics.Typeface;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import com.autowp.can.CanAdapter;
 import com.citroen.commands.EnsureSource;
 import com.citroen.handlers.Source;
 import com.citroen.handlers.ToastRadioFrequency;
 import com.citroen.handlers.radioActivity.*;
 import com.jvit.bus.Signal;
 
-public class RadioActivity extends ServiceConnectedActivity implements CanReaderService.OnMonitorChangedListener {
+public class RadioActivity extends ServiceConnectedActivity implements CanReaderService.OnMonitorChangedListener,
+        CanReaderService.OnConnectionStateChangedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,8 +26,7 @@ public class RadioActivity extends ServiceConnectedActivity implements CanReader
 
 //        TextView radioFreq = (TextView) findViewById(R.id.radioFrequency);
 //        radioFreq.setText(ToastRadioFrequency.getInstance().getLastValue());
-        EnsureSource command = new EnsureSource(canReaderService,"Tuner");
-        command.execute();
+
     }
 
     protected void setHandlers() {
@@ -54,14 +55,16 @@ public class RadioActivity extends ServiceConnectedActivity implements CanReader
 
     @Override
     protected void afterConnect() {
-        canReaderService.addListener(this);
+        canReaderService.addListener((CanReaderService.OnMonitorChangedListener) this);
+        canReaderService.addListener((CanReaderService.OnConnectionStateChangedListener) this);
         setHandlers();
     }
 
     @Override
     protected void beforeDisconnect() {
         unsetHandlers();
-        canReaderService.removeListener(this);
+        canReaderService.removeListener((CanReaderService.OnConnectionStateChangedListener) this);
+        canReaderService.removeListener((CanReaderService.OnMonitorChangedListener) this);
     }
 
     @Override
@@ -77,5 +80,13 @@ public class RadioActivity extends ServiceConnectedActivity implements CanReader
     @Override
     public void handleSpeedChanged(double speed) {
 
+    }
+
+    @Override
+    public void handleConnectedStateChanged(CanAdapter.ConnectionState connection) {
+        if (connection == CanAdapter.ConnectionState.CONNECTED) {
+            EnsureSource command = new EnsureSource(canReaderService,"Tuner");
+            command.execute();
+        }
     }
 }
